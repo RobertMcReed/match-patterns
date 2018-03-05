9c635c93b08585a8b652aebf06e92c293a2a72f5
Robert Reed <robert.mc.reed@gmail.com>
2018-03-04 22:26:47 -0800

    structure of path matcher

--- index.js
+++ index.js
@@ -2,7 +2,9 @@
 
 const util = require('./lib/util.js');
 const readStdin = require('./lib/read-stdin.js');
+const parseData = require('./lib/parse-data.js');
 
 readStdin()
-  .then(util.logRaw)
+  .then(parseData)
+  .then(util.logFormatted)
   .catch(console.log);
