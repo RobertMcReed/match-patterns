9fbe99072ea1833efee0abd1984b7b5db1244e81
Robert Reed <robert.mc.reed@gmail.com>
2018-03-06 22:27:29 -0800

    actually finally truly finding matches!

--- index.js
+++ index.js
@@ -3,8 +3,12 @@
 const util = require('./lib/util.js');
 const readStdin = require('./lib/read-stdin.js');
 const parseData = require('./lib/parse-data.js');
+const findMatches = require('./lib/find-matches.js');
 
 readStdin()
   .then(parseData)
+  .then(findMatches)
+  // .then(util.printSeparately)
   .then(util.logFormatted)
+  // .then(util.logRaw)
   .catch(console.log);
