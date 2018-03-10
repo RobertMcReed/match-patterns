09bb3414a5a7b7f959991931da4ea16962729525
Robert Reed <robert.mc.reed@gmail.com>
2018-03-09 23:04:20 -0800

    solution almost there, need to fix split issue

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 'use strict';
 
 const util = require('./lib/util.js');
+const helpers = require('./lib/helpers.js');
 const readStdin = require('./lib/read-stdin.js');
 const parseData = require('./lib/parse-data.js');
 const findMatches = require('./lib/find-matches.js');
+const findBestMatches = require('./lib/find-best-matches.js');
 
 readStdin()
   .then(parseData)
   .then(findMatches)
-  // .then(util.printSeparately)
-  .then(util.logFormatted)
-  // .then(util.logRaw)
+  .then(findBestMatches)
+  .then(helpers.joinAsLines)
+  .then(console.log)
   .catch(console.log);
